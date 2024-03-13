56c92e2c62ad937a9a9f1a6e2aeeee3734060dd7
anthony <anthonymccann90@gmail.com>
2024-03-13 21:48:15 +0000

    adding docker volume for file persistence

--- SimpleIptvManager/AppConfiguration.cs
+++ SimpleIptvManager/AppConfiguration.cs
@@ -2,12 +2,14 @@
 {
     public static class AppConfiguration
     {
+        public static bool IsContainer => string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("IsContainer")) ? false : 
+            Environment.GetEnvironmentVariable("IsContainer").Equals("true");
         public static string ApplicationName => "SimpleIptvManager";
-        public static string AppDataDirectory => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ApplicationName);
+        public static string AppDataDirectory => IsContainer ? "/config" :
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ApplicationName);
         public static string PlaylistAndGuideDirectory => Path.Combine(AppDataDirectory, "playlists");
         public static string ProgramGuideSourcesDirectory => Path.Combine(AppDataDirectory, "epgsources");
         public static string DbDirectory => Path.Combine(AppDataDirectory, "db");
         public static string DbName => "simple.db";
-
     }
 }
